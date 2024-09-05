14f8beca8bbebb140104fd907cf4bd1edc5773a9
Thomas Wright <thomaswright1@gmail.com>
2024-09-06 04:06:30 +0900

    add demo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://thomaswright.github.io/arc-agi-solutions",
   plugins: [
     react({
       include: ["**/*.res.mjs"],
